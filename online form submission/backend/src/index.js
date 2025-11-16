5098c8ed7bfdc93b9365580d152e722379cbf4ff
Suhail Ahmed <suhailbhand7@gmail.com>
2025-11-16 21:06:35 +0500

    Index file(Main File)

--- online form submission/backend/src/index.js
+++ online form submission/backend/src/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
-import authRoutes from "./routes/auth.routes.js"; // <-- import auth routes
+import authRoutes from "./routes/auth.routes.js";
 
 dotenv.config({ path: "../.env" });
 
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 // Mount auth routes
-app.use("/api/auth", authRoutes); // <-- this is essential
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("Backend API is running...");
